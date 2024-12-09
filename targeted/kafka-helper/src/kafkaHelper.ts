62b37d71fd6f66e6442b0b00ad6186bfb3202488
adamviktora <viktora.adam@gmail.com>
2024-12-09 14:16:13 +0100

    feat: add kafka admin helper

---
+++ targeted/kafka-helper/src/kafkaHelper.ts
@@ -0,0 +1,32 @@
+import { Admin, ITopicConfig } from 'kafkajs';
+
+export const printTopicInfo = async (admin: Admin, topicName: string) => {
+  const metadata = await admin.fetchTopicMetadata({ topics: [topicName] });
+
+  console.log(metadata);
+  console.log(metadata.topics[0].partitions);
+};
+
+export const createTopic = async (
+  admin: Admin,
+  topicName: string,
+  partitionsCount: number
+) => {
+  const topicsToCreate: ITopicConfig[] = [
+    {
+      topic: topicName,
+      numPartitions: partitionsCount,
+      replicationFactor: 1,
+    },
+  ];
+
+  const topicsCreated = await admin.createTopics({
+    topics: topicsToCreate,
+  });
+
+  if (topicsCreated) {
+    console.log(`Topic ${topicName} created successfully`);
+  } else {
+    console.log(`Topic ${topicName} already exists`);
+  }
+};
